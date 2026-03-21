4ba89249a6ab1cf01fe9f25996b9321ff47da03a
Omkar Nanajkar <nomkar2131@gmail.com>
2026-03-21 18:10:23 +0530

    zlib test comperss and decompress verified

--- tests/zlib/main.c
+++ tests/zlib/main.c
@@ -1,189 +1,58 @@
-#include <stdio.h>
+#include <string.h>
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/fs/fs.h>
-#include <zephyr/fs/littlefs.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/storage/flash_map.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <zlib.h>
 
-#include "example.h"
-#include "infcover.h"
+LOG_MODULE_REGISTER(zlib_test, LOG_LEVEL_INF);
 
-#define MAX_STACK_SIZE 8192
-#define THREAD_PRIORITY 7
+int main(void) {
+  const char *test_string = "zlib compression test";
 
-K_THREAD_STACK_DEFINE(thread_stack, MAX_STACK_SIZE);
+  uLong source_len = strlen(test_string) + 1;
+  uLong dest_len = compressBound(source_len);
+  unsigned char *dest_buffer = k_malloc(dest_len);
+  unsigned char *uncomp_buffer = k_malloc(source_len);
 
+  if (!dest_buffer || !uncomp_buffer) {
+    LOG_ERR("Failed to allocate memory for zlib test!");
+    k_free(dest_buffer);
+    k_free(uncomp_buffer);
+    return 1;
+  }
 
-void runTest(k_thread_entry_t entry)
-{
-    struct k_thread thread;
-    k_tid_t id;
-    id = k_thread_create(&thread, thread_stack, MAX_STACK_SIZE, entry, NULL, NULL, NULL, THREAD_PRIORITY, K_INHERIT_PERMS, K_NO_WAIT);
-    k_thread_join(id, K_FOREVER);
-}
+  LOG_INF("Starting zlib test...");
+  LOG_INF("Source String: %s", test_string);
+  LOG_INF("Source Length: %lu", source_len);
 
-// Littlefs
-LOG_MODULE_REGISTER(main);
+  // 1. Compress
+  int res =
+      compress(dest_buffer, &dest_len, (const Bytef *)test_string, source_len);
+  if (res != Z_OK) {
+    LOG_ERR("Compression failed with error code: %d", res);
+    goto cleanup;
+  }
+  LOG_INF("Compressed Length: %lu", dest_len);
 
-/* Matches LFS_NAME_MAX */
-#define MAX_PATH_LEN 255
-#define TEST_FILE_SIZE 547
+  // 2. Decompress
+  uLong uncomp_len = source_len;
+  res = uncompress(uncomp_buffer, &uncomp_len, dest_buffer, dest_len);
+  if (res != Z_OK) {
+    LOG_ERR("Decompression failed with error code: %d", res);
+    goto cleanup;
+  }
+  LOG_INF("Decompressed Length: %lu", uncomp_len);
+  LOG_INF("Decompressed String: %s", uncomp_buffer);
 
-static int lsdir(const char *path)
-{
-	int res;
-	struct fs_dir_t dirp;
-	static struct fs_dirent entry;
+  // 3. Verify
+  if (strcmp(test_string, (char *)uncomp_buffer) == 0) {
+    LOG_INF("zlib Round-trip: SUCCESS");
+  } else {
+    LOG_ERR("zlib Round-trip: FAILURE (Mismatch)");
+  }
 
-	fs_dir_t_init(&dirp);
+cleanup:
+  k_free(dest_buffer);
+  k_free(uncomp_buffer);
 
-	/* Verify fs_opendir() */
-	res = fs_opendir(&dirp, path);
-	if (res) {
-		LOG_ERR("Error opening dir %s [%d]\n", path, res);
-		return res;
-	}
-
-	LOG_PRINTK("\nListing dir %s ...\n", path);
-	for (;;) {
-		/* Verify fs_readdir() */
-		res = fs_readdir(&dirp, &entry);
-
-		/* entry.name[0] == 0 means end-of-dir */
-		if (res || entry.name[0] == 0) {
-			if (res < 0) {
-				LOG_ERR("Error reading dir [%d]\n", res);
-			}
-			break;
-		}
-
-		if (entry.type == FS_DIR_ENTRY_DIR) {
-			LOG_PRINTK("[DIR ] %s\n", entry.name);
-		} else {
-			LOG_PRINTK("[FILE] %s (size = %zu)\n",
-				   entry.name, entry.size);
-		}
-	}
-
-	/* Verify fs_closedir() */
-	fs_closedir(&dirp);
-
-	return res;
-}
-
-#ifdef CONFIG_APP_LITTLEFS_STORAGE_FLASH
-static int littlefs_flash_erase(unsigned int id)
-{
-	const struct flash_area *pfa;
-	int rc;
-
-	rc = flash_area_open(id, &pfa);
-	if (rc < 0) {
-		LOG_ERR("FAIL: unable to find flash area %u: %d\n",
-			id, rc);
-		return rc;
-	}
-
-	LOG_PRINTK("Area %u at 0x%x on %s for %u bytes\n",
-		   id, (unsigned int)pfa->fa_off, pfa->fa_dev->name,
-		   (unsigned int)pfa->fa_size);
-
-	/* Optional wipe flash contents */
-	if (IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
-		rc = flash_area_flatten(pfa, 0, pfa->fa_size);
-		LOG_ERR("Erasing flash area ... %d", rc);
-	}
-
-	flash_area_close(pfa);
-	return rc;
-}
-#define PARTITION_NODE DT_NODELABEL(lfs1)
-
-#if DT_NODE_EXISTS(PARTITION_NODE)
-FS_FSTAB_DECLARE_ENTRY(PARTITION_NODE);
-#else /* PARTITION_NODE */
-FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
-static struct fs_mount_t lfs_storage_mnt = {
-	.type = FS_LITTLEFS,
-	.fs_data = &storage,
-	.storage_dev = (void *)FIXED_PARTITION_ID(storage_partition),
-	.mnt_point = "/lfs",
-};
-#endif /* PARTITION_NODE */
-
-	struct fs_mount_t *mountpoint =
-#if DT_NODE_EXISTS(PARTITION_NODE)
-		&FS_FSTAB_ENTRY(PARTITION_NODE)
-#else
-		&lfs_storage_mnt
-#endif
-		;
-
-static int littlefs_mount(struct fs_mount_t *mp)
-{
-	int rc;
-
-	rc = littlefs_flash_erase((uintptr_t)mp->storage_dev);
-	if (rc < 0) {
-		return rc;
-	}
-
-	/* Do not mount if auto-mount has been enabled */
-#if !DT_NODE_EXISTS(PARTITION_NODE) ||						\
-	!(FSTAB_ENTRY_DT_MOUNT_FLAGS(PARTITION_NODE) & FS_MOUNT_FLAG_AUTOMOUNT)
-	rc = fs_mount(mp);
-	if (rc < 0) {
-		LOG_PRINTK("FAIL: mount id %" PRIuPTR " at %s: %d\n",
-		       (uintptr_t)mp->storage_dev, mp->mnt_point, rc);
-		return rc;
-	}
-	LOG_PRINTK("%s mount: %d\n", mp->mnt_point, rc);
-#else
-	LOG_PRINTK("%s automounted\n", mp->mnt_point);
-#endif
-
-	return 0;
-}
-#endif /* CONFIG_APP_LITTLEFS_STORAGE_FLASH */
-
-
-
-int main()
-{
-    char fname1[MAX_PATH_LEN];
-	struct fs_statvfs sbuf;
-	int rc;
-
-	LOG_PRINTK("Initializing file system with littlefs\n");
-
-	rc = littlefs_mount(mountpoint);
-	if (rc < 0) {
-		return 0;
-	}
-
-	snprintf(fname1, sizeof(fname1), "%s/test.txt", mountpoint->mnt_point);
-
-	rc = fs_statvfs(mountpoint->mnt_point, &sbuf);
-	if (rc < 0) {
-		LOG_PRINTK("FAIL: statvfs: %d\n", rc);
-		goto out;
-	}
-
-	LOG_PRINTK("%s: bsize = %lu ; frsize = %lu ;"
-		   " blocks = %lu ; bfree = %lu\n",
-		   mountpoint->mnt_point,
-		   sbuf.f_bsize, sbuf.f_frsize,
-		   sbuf.f_blocks, sbuf.f_bfree);
-	
-	runTest(example_main);
-	runTest(infcover_main);
-
-out:
-	rc = fs_unmount(mountpoint);
-	LOG_PRINTK("%s unmount: %d\n", mountpoint->mnt_point, rc);
-    return 0;
+  return 0;
 }

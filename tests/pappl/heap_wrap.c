942563694ef286b0474d2479761b205fed74d178
nomkar24 <nomkar2131@gmail.com>
2026-07-01 19:19:14 +0530

    Enabled thread analyzer and increased log stack size to 8kb

--- tests/pappl/heap_wrap.c
+++ tests/pappl/heap_wrap.c
@@ -1,7 +1,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/multi_heap/shared_multi_heap.h>
+#include <zephyr/spinlock.h>
 
-static K_MUTEX_DEFINE(smh_wrap_mutex);
+static struct k_spinlock smh_wrap_lock;
 
 extern void *__real_shared_multi_heap_alloc(enum shared_multi_heap_attr attr, size_t bytes);
 extern void *__real_shared_multi_heap_aligned_alloc(enum shared_multi_heap_attr attr, size_t align, size_t bytes);
@@ -11,33 +12,33 @@ extern void __real_shared_multi_heap_free(void *block);
 void *__wrap_shared_multi_heap_alloc(enum shared_multi_heap_attr attr, size_t bytes)
 {
   void *ptr;
-  k_mutex_lock(&smh_wrap_mutex, K_FOREVER);
+  k_spinlock_key_t key = k_spin_lock(&smh_wrap_lock);
   ptr = __real_shared_multi_heap_alloc(attr, bytes);
-  k_mutex_unlock(&smh_wrap_mutex);
+  k_spin_unlock(&smh_wrap_lock, key);
   return ptr;
 }
 
 void *__wrap_shared_multi_heap_aligned_alloc(enum shared_multi_heap_attr attr, size_t align, size_t bytes)
 {
   void *ptr;
-  k_mutex_lock(&smh_wrap_mutex, K_FOREVER);
+  k_spinlock_key_t key = k_spin_lock(&smh_wrap_lock);
   ptr = __real_shared_multi_heap_aligned_alloc(attr, align, bytes);
-  k_mutex_unlock(&smh_wrap_mutex);
+  k_spin_unlock(&smh_wrap_lock, key);
   return ptr;
 }
 
 void *__wrap_shared_multi_heap_realloc(enum shared_multi_heap_attr attr, void *ptr, size_t bytes)
 {
   void *new_ptr;
-  k_mutex_lock(&smh_wrap_mutex, K_FOREVER);
+  k_spinlock_key_t key = k_spin_lock(&smh_wrap_lock);
   new_ptr = __real_shared_multi_heap_realloc(attr, ptr, bytes);
-  k_mutex_unlock(&smh_wrap_mutex);
+  k_spin_unlock(&smh_wrap_lock, key);
   return new_ptr;
 }
 
 void __wrap_shared_multi_heap_free(void *block)
 {
-  k_mutex_lock(&smh_wrap_mutex, K_FOREVER);
+  k_spinlock_key_t key = k_spin_lock(&smh_wrap_lock);
   __real_shared_multi_heap_free(block);
-  k_mutex_unlock(&smh_wrap_mutex);
+  k_spin_unlock(&smh_wrap_lock, key);
 }

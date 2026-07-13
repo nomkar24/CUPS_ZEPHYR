5f3bd9d9f6442d69d6d6511db506f436cc1e8664
nomkar24 <nomkar2131@gmail.com>
2026-07-13 22:12:44 +0530

    Increased CUPS and PAPPL thread size, Redced CONFIG_WIFI_NM_MAX_MANAGED_INTERFACES to 1

--- tests/pappl/heap_wrap.c
+++ tests/pappl/heap_wrap.c
@@ -42,3 +42,57 @@ void __wrap_shared_multi_heap_free(void *block)
   __real_shared_multi_heap_free(block);
   k_spin_unlock(&smh_wrap_lock, key);
 }
+
+// Override ROM string functions to prevent LoadStoreError crashes when accessing PSRAM on ESP32-S3.
+// The ESP32-S3 ROM libc implementations use optimized assembly that can cause alignment/bus errors on SPIRAM.
+int __wrap_strcmp(const char *s1, const char *s2)
+{
+  while (*s1 && (*s1 == *s2))
+  {
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+}
+
+size_t __wrap_strlen(const char *s)
+{
+  const char *p = s;
+  while (*p)
+    p++;
+  return p - s;
+}
+
+char *__wrap_strcpy(char *dest, const char *src)
+{
+  char *d = dest;
+  while ((*d++ = *src++))
+    ;
+  return dest;
+}
+
+int __wrap_strncmp(const char *s1, const char *s2, size_t n)
+{
+  if (n == 0)
+    return 0;
+  while (n-- > 0 && *s1 == *s2)
+  {
+    if (n == 0 || *s1 == '\0')
+      break;
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+}
+
+char *__wrap_strchr(const char *s, int c)
+{
+  while (*s != (char)c)
+  {
+    if (!*s)
+      return NULL;
+    s++;
+  }
+  return (char *)s;
+}
+
